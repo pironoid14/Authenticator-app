69ec9907d985e0493245ef477619fc9f6f1d7635
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-02 15:57:49 +0100

    correction to code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Login } from "./login"
+
+import { Login } from "./login";
+
 function App() {
- 
+
+  
 
   return (
     <>
       <div className="text-black justify-center flex h-[100vh] items-center bg-gradient-to-b from-blue-700 via-teal-600 to-indigo-700">
-     <Login/>
+        <Login/>
       </div>
     </>
   )
--- src/login.tsx
+++ src/login.tsx
@@ -1,21 +1,29 @@
+import { useState } from "react";
 
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {};
   return (
     <div className="bg-black border border-black rounded-md shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-40 relative">
         <h1 className="text-4xl font bold text-center">Login form</h1>
         <form action="">
             <div className="relative my-4">
             <label htmlFor="email"className="text-sm peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-4 top-3">Email</label>
-            <input type="email" className="block py-2.5 px-0 text-base text-white bg-transparent w-72 border-0 border-b-2 border-neutral-900" placeholder=""/>
-                
+            <input type="email" className="block py-2.5 px-0 text-base text-white bg-transparent w-72 border-0 border-b-2 border-neutral-900" placeholder=""name="email" value={email}
+              onChange={(e) => setEmail(e.target.value)}/>
+              
             </div>
             <div className="relative my-4">
             <label htmlFor="email"className="text-sm text-black peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-4 top-3 ">Password</label>
-            <input type="password" className=" block py-2.5 px-0 text-base text-white bg-transparent w-72 border-0 border-b-2 border-neutral-900" placeholder=""/>
+            <input type="password" className=" block py-2.5 px-0 text-base text-white bg-transparent w-72 border-0 border-b-2 border-neutral-900" placeholder="" name="password"value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)}}/>
                 
             </div>
-            <button type="submit" className="w-full mb-4 text-[18px] roundedbg-black-600 py-2 hover:bg-teal-800">Submit</button>
+            <button type="submit" className="w-full mb-4 text-[18px] roundedbg-black-600 py-2 hover:bg-teal-800"onClick={handleLogin}>Submit</button>
         </form>
     </div>
   )
