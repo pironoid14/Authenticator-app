6504fcee820e48c44922f464232363c091af7477
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-05 09:25:35 +0100

    changes made to register

--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -31,42 +31,43 @@ export const Register = () => {
     }
   };
   return (
-    <div className="bg-red-500 grid place-content-center">
+    <div className="bg-amber-300 flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-md bg-white rounded-lg p-6 shadow-md">
       <h1>MEW</h1>
-      <form action="" onSubmit={handleRegister}>
-        <div>
-          <label htmlFor="" className="text-lg text-red-500">First name</label>
-          <input type="text" name="" className="rounded-s-lg" value={name}
+      <form action="" onSubmit={handleRegister} className="space-y-4">
+        <div className="mb-4">
+          <label htmlFor=""  className="block text-lg text-gray-700">First name</label>
+          <input type="text" name=""  className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={name}
             onChange={(e) => setName(e.target.value)}
           />
           {errors.name && <p>{errors.name}</p>}
 
           <label htmlFor="">Last Name</label>
-          <input type="text" name="" value={name}
+          <input type="text" name="" className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={name}
             onChange={(e) => setName(e.target.value)}
           />
           {errors.name && <p>{errors.name}</p>}
 
           <label htmlFor="">Other Names</label>
-          <input type="text" name="" value={name}
+          <input type="text" name="" className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={name}
             onChange={(e) => setName(e.target.value)}
           />
           {errors.name && <p>{errors.name}</p>}
 
           <label htmlFor="">Phone Number</label>
-          <input type="number" name="" id="" value={number}
+          <input type="number" name="" id="" className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={number}
             onChange={(e) => setNumber(e.target.value)}
           />
           {errors.number && <p>{errors.number}</p>}
 
           <label htmlFor="">Email</label>
-          <input type="email" name="email" id="" value={email}
+          <input type="email" name="email" id="" className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
           {errors.email && <p>{errors.email}</p>}
 
           <label htmlFor="">Password</label>
-          <input type="password" name="" id="" value={password}
+          <input type="password" name="" id="" className="rounded-md shadow-sm w-full border focus:outline-none focus:ring mt-1 focus:border-blue-500 px-3 py-2" value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
           {errors.password && <p>{errors.password}</p>}
@@ -75,12 +76,13 @@ export const Register = () => {
             <LiaToggleOffSolid />
           </div>
 
-          <button type="button" onClick={handleRegister} className=" bg-blue-700 rounded-s-lg ">Register</button>
-          <Link to="/login">Already have an account? Login</Link>
-          <Link to="/register">Do you have an Account Register</Link>
+          <button type="button" onClick={handleRegister} className=" bg-blue-700 rounded-lg px-4 py-2 hover:bg-stone-600 ">Register</button>
+          <Link to="/login" className="text-blue-800">Already have an account? Login</Link>
+         
           <Link to = "/Home"></Link>
         </div>
       </form>
+      </div>
     </div>
   )
 }
