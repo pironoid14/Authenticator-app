e28189680d6bd1400cd4e062d5fe78d77f4c6e6d
pironoid14 <56125168+pironoid14@users.noreply.github.com>
2024-08-10 22:05:20 +0100

    added otp to register

--- src/Pages/register.tsx
+++ src/Pages/register.tsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { LiaToggleOffSolid } from "react-icons/lia";
 import { useState } from "react";
+import  OtpInput  from "react-otp-input";
 
 export const Register = () => {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [otp, setOtp] = useState("")
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const validate = () => {
@@ -17,23 +19,28 @@ export const Register = () => {
     else if (!/\S+@\S+\.\S+/.test(email)) newErrors.email = "Email is invalid";
     if (!password) newErrors.password = "Password is required";
     else if (password.length < 6) newErrors.password = "Password must be at least 6 characters";
+    
 
     return newErrors;
   };
 
-  const handleRegister = () => {
+  
+
+  const handleRegister = (e:React.FormEvent) => {
+    e.preventDefault();
     const validationErrors = validate();
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
     } else {
       setErrors({});
-      console.log({ name, number, email, password });
+      console.log({ name, number, email, password,otp });
     }
   };
   return (
     <div className="bg-amber-300 flex justify-center items-center min-h-screen">
       <div className="w-full max-w-md bg-white rounded-lg p-6 shadow-md">
-      <h1>MEW</h1>
+      <h1 className="text-center text-3xl">MEW</h1>
+      <h3 className="text-2xl text-center">Registration form</h3>
       <form action="" onSubmit={handleRegister} className="space-y-4">
         <div className="mb-4">
           <label htmlFor=""  className="block text-lg text-gray-700">First name</label>
@@ -72,6 +79,30 @@ export const Register = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
           {errors.password && <p className="text-red-500">{errors.password}</p>}
+          <div>
+            <label className="block text-lg text-gray-700">OTP</label>
+            <OtpInput
+      value={otp}
+      onChange={setOtp}
+      numInputs={6}
+      renderSeparator={<span>-</span>}
+      renderInput={(props) => <input {...props} />}
+       className="flex justify-center mt-2"
+              inputStyle={{
+                width: "3rem",
+                height: "3rem",
+                margin: "0 0.5rem",
+                fontSize: "1.5rem",
+                borderRadius: "0.5rem",
+                border: "1px solid rgba(0,0,0,0.3)"
+              }}
+              focusStyle={{
+                border: "1px solid #blue-700"
+              }}
+    />
+     
+          </div>
+
 
           <div>
             <LiaToggleOffSolid />
